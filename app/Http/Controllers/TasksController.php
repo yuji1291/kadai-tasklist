23ba0371d8ab39eb89d84b815efab84a2b1d6cc2
yuji.sese <kuronekoyamato59@gmail.com>
2019-11-02 03:45:00 +0000

    status column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,8 +43,13 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-     {
+    {
+        $this->validate($request, [
+            'status' => 'required|max:10', 
+            'content' => 'required|max:191',
+        ]);
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -88,8 +93,13 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
+     {
+        $this->validate($request, [
+            'status' => 'required|max:10', 
+            'content' => 'required|max:191',
+        ]);
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
